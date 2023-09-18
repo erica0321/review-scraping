2780dd56c3b5f427fdf9990eb8ded854b7f5cad4
erica0321 <hyunahrho29@gmail.com>
2023-09-18 14:42:22 +0900

    modified:   index.ts

--- index.ts
+++ index.ts
@@ -6,13 +6,13 @@ import { reviewScraperFactory } from './src/reviewScraperFactory.js'
 //ably 주소
 // 'https://m.a-bly.com/goods/2982671'
 
-//blogpay 주고
+//blogpay 주소
 // 'https://juuvuv.shop.blogpay.co.kr/good/product_view?goodNum=203092667'
 
 //brandi 주소
 // 'https://www.brandi.co.kr/products/106362123?tab=2'
 
 let reviewScraper = await reviewScraperFactory(
-  'http://www.10x10.co.kr/shopping/category_prd.asp?itemid=5616003&gaparam=bestaward_b_3'
+  'https://www.brandi.co.kr/products/106362123?tab=2'
 )
 console.log(await reviewScraper.scrap())
